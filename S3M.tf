6c477b455f6faecd97ee5effd849e96e73c7fd43
Sujay <42913490+hsujay80@users.noreply.github.com>
2021-02-06 12:02:02 +0530

    Update S3M.tf

--- S3M.tf
+++ S3M.tf
@@ -1,5 +1,5 @@
 module "foo"{
-    source = "https://github.com/hsujay80/Terraform-Modules/blob/main/S3.tf"
+    source = "https://github.com/hsujay80/Terraform-Modules/tree/main"
     bucket_name = "Sujay"
 
 }
